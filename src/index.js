4b404cfcabad326f91750f26f956147bed702cfc
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-03-07 21:03:01 -0500

    Now using static auth library.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
-var AUTH = '';
-var ready = false;
-var interval = null;
-var send = function() {
-	$('iframe')[0].contentWindow.postMessage(undefined, 'https://login.belowaverage.org');
-};
-onmessage = function(e) {
-	try {
-		if(!ready && e.data == 'ready') {
-			clearInterval(interval);
-			ready = true;
-		} else if(ready && e.data.length == 32) {
-			AUTH = e.data;
-			$('iframe').remove();
-			$('#main').show();
-			
-			$('input[type=hidden][name=AUTH]').val(AUTH);
-			
-			$.ajax({
-				url: 'https://api.belowaverage.org/v1/adphoto/',
-				method: 'post',
-				xhrFields: {
-					responseType: 'blob',
-				},
-				data: {
-					'AUTH': AUTH
-				},
-				success: function(data) {
-					$('img#profile').attr('src', URL.createObjectURL(data));
-				}
-			});
+AUTH.authenticated = function() {
+	$('#main').show();
+	$('input[type=hidden][name=AUTH]').val(AUTH.token);		
+	$.ajax({
+		url: 'https://api.belowaverage.org/v1/adphoto/',
+		method: 'post',
+		xhrFields: {
+			responseType: 'blob',
+		},
+		data: {
+			'AUTH': AUTH.token
+		},
+		success: function(data) {
+			$('img#profile').attr('src', URL.createObjectURL(data));
 		}
-	} catch(e) {
-		console.warn('Login IFRAME not ready. Trying again.');
-	}
-};
-$(document).ready(function() {
-	interval = setInterval(send, 100);
-});
+	});
+}
